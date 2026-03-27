824e1a50634ef3c81535c3f58f0e03871a7423ac
Danny O'Brien <danny@spesh.com>
2026-03-27 16:46:31 -0700

    feat: add backend interface and local-file backend

---
+++ src/backends/local.js
@@ -0,0 +1,28 @@
+import fs from 'node:fs'
+import path from 'node:path'
+import { pipeline } from 'node:stream/promises'
+
+export class LocalBackend {
+  constructor({ outputDir }) {
+    this.name = 'local'
+    this.outputDir = outputDir
+  }
+
+  async init() {
+    fs.mkdirSync(this.outputDir, { recursive: true })
+  }
+
+  async hasContent(rootCid) {
+    const filePath = path.join(this.outputDir, `${rootCid}.car`)
+    return fs.existsSync(filePath)
+  }
+
+  async importCar(rootCid, stream) {
+    fs.mkdirSync(this.outputDir, { recursive: true })
+    const filePath = path.join(this.outputDir, `${rootCid}.car`)
+    const dest = fs.createWriteStream(filePath)
+    await pipeline(stream, dest)
+  }
+
+  async close() {}
+}
